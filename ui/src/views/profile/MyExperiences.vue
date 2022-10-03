9480307ae46c9ba5e5141536f992beb199fcaa0d
KS LIM <potatoscript@yahoo.com>
2022-10-04 08:37:05 +0900

    adding job history

--- ui/src/views/profile/MyExperiences.vue
+++ ui/src/views/profile/MyExperiences.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table table-striped">
+  <table class="table table-striped" style="font-size: 12px">
     <thead>
       <tr>
         <th>{{ $t("message.Period") }}</th>
@@ -11,53 +11,223 @@
       </tr>
     </thead>
     <tbody>
+      <!--Singapore-->
       <tr>
-        <td>{{ $t("job.Period1") }}</td>
-        <td>{{ $t("job.Company1") }}</td>
+        <td>
+          <p>{{ $t("job.Period1.1") }}</p>
+          <p>{{ $t("job.Period1.2") }}</p>
+          <p>{{ $t("job.Period1.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company1.1") }}</p>
+          <p>{{ $t("job.Company1.2") }}</p>
+        </td>
         <td>{{ $t("job.Location1") }}</td>
-        <td>{{ $t("job.JobScope1") }}</td>
-        <td>{{ $t("job.Outcome1") }}</td>
-        <td>{{ $t("job.Tools1") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope1.1") }}</p>
+          <p>{{ $t("job.JobScope1.2") }}</p>
+          <p>{{ $t("job.JobScope1.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome1.1") }}</p>
+          <p>{{ $t("job.Outcome1.2") }}</p>
+          <p>{{ $t("job.Outcome1.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools1.1") }}</p>
+          <p>{{ $t("job.Tools1.2") }}</p>
+          <p>{{ $t("job.Tools1.3") }}</p>
+          <p>{{ $t("job.Tools1.4") }}</p>
+        </td>
       </tr>
+      <!--Japan(Tottori)-->
       <tr>
-        <td>{{ $t("job.Period2") }}</td>
-        <td>{{ $t("job.Company2") }}</td>
+        <td>
+          <p>{{ $t("job.Period2.1") }}</p>
+          <p>{{ $t("job.Period2.2") }}</p>
+          <p>{{ $t("job.Period2.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company2.1") }}</p>
+          <p>{{ $t("job.Company2.2") }}</p>
+        </td>
         <td>{{ $t("job.Location2") }}</td>
-        <td>{{ $t("job.JobScope2") }}</td>
-        <td>{{ $t("job.Outcome2") }}</td>
-        <td>{{ $t("job.Tools12") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope2.1") }}</p>
+          <p>{{ $t("job.JobScope2.2") }}</p>
+          <p>{{ $t("job.JobScope2.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome2.1") }}</p>
+          <p>{{ $t("job.Outcome2.2") }}</p>
+          <p>{{ $t("job.Outcome2.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools2.1") }}</p>
+          <p>{{ $t("job.Tools2.2") }}</p>
+          <p>{{ $t("job.Tools2.3") }}</p>
+          <p>{{ $t("job.Tools2.4") }}</p>
+        </td>
       </tr>
+      <!--Thailand-->
       <tr>
-        <td>{{ $t("job.Period3") }}</td>
-        <td>{{ $t("job.Company3") }}</td>
+        <td>
+          <p>{{ $t("job.Period3.1") }}</p>
+          <p>{{ $t("job.Period3.2") }}</p>
+          <p>{{ $t("job.Period3.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company3.1") }}</p>
+          <p>{{ $t("job.Company3.2") }}</p>
+        </td>
         <td>{{ $t("job.Location3") }}</td>
-        <td>{{ $t("job.JobScope3") }}</td>
-        <td>{{ $t("job.Outcome3") }}</td>
-        <td>{{ $t("job.Tools3") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope3.1") }}</p>
+          <p>{{ $t("job.JobScope3.2") }}</p>
+          <p>{{ $t("job.JobScope3.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome3.1") }}</p>
+          <p>{{ $t("job.Outcome3.2") }}</p>
+          <p>{{ $t("job.Outcome3.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools3.1") }}</p>
+          <p>{{ $t("job.Tools3.2") }}</p>
+          <p>{{ $t("job.Tools3.3") }}</p>
+          <p>{{ $t("job.Tools3.4") }}</p>
+        </td>
       </tr>
+      <!--Japan(Tottori)-->
       <tr>
-        <td>{{ $t("job.Period4") }}</td>
-        <td>{{ $t("job.Company4") }}</td>
+        <td>
+          <p>{{ $t("job.Period4.1") }}</p>
+          <p>{{ $t("job.Period4.2") }}</p>
+          <p>{{ $t("job.Period4.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company4.1") }}</p>
+          <p>{{ $t("job.Company4.2") }}</p>
+        </td>
         <td>{{ $t("job.Location4") }}</td>
-        <td>{{ $t("job.JobScope4") }}</td>
-        <td>{{ $t("job.Outcome4") }}</td>
-        <td>{{ $t("job.Tools4") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope4.1") }}</p>
+          <p>{{ $t("job.JobScope4.2") }}</p>
+          <p>{{ $t("job.JobScope4.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome4.1") }}</p>
+          <p>{{ $t("job.Outcome4.2") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools4.1") }}</p>
+          <p>{{ $t("job.Tools4.2") }}</p>
+          <p>{{ $t("job.Tools4.3") }}</p>
+          <p>{{ $t("job.Tools4.4") }}</p>
+        </td>
       </tr>
+
+      <!--Japan(Tottori)-->
       <tr>
-        <td>{{ $t("job.Period5") }}</td>
-        <td>{{ $t("job.Company5") }}</td>
+        <td>
+          <p>{{ $t("job.Period5.1") }}</p>
+          <p>{{ $t("job.Period5.2") }}</p>
+          <p>{{ $t("job.Period5.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company5.1") }}</p>
+          <p>{{ $t("job.Company5.2") }}</p>
+        </td>
         <td>{{ $t("job.Location5") }}</td>
-        <td>{{ $t("job.JobScope5") }}</td>
-        <td>{{ $t("job.Outcome5") }}</td>
-        <td>{{ $t("job.Tools5") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope5.1") }}</p>
+          <p>{{ $t("job.JobScope5.2") }}</p>
+          <p>{{ $t("job.JobScope5.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome5.1") }}</p>
+          <p>{{ $t("job.Outcome5.2") }}</p>
+          <p>{{ $t("job.Outcome5.3") }}</p>
+          <p>{{ $t("job.Outcome5.4") }}</p>
+          <p>{{ $t("job.Outcome5.5") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools5.1") }}</p>
+          <p>{{ $t("job.Tools5.2") }}</p>
+          <p>{{ $t("job.Tools5.3") }}</p>
+          <p>{{ $t("job.Tools5.4") }}</p>
+          <p>{{ $t("job.Tools5.5") }}</p>
+          <p>{{ $t("job.Tools5.6") }}</p>
+          <p>{{ $t("job.Tools5.7") }}</p>
+        </td>
       </tr>
+
+      <!--Indonesia-->
       <tr>
-        <td>{{ $t("job.Period6") }}</td>
-        <td>{{ $t("job.Company6") }}</td>
+        <td>
+          <p>{{ $t("job.Period6.1") }}</p>
+          <p>{{ $t("job.Period6.2") }}</p>
+          <p>{{ $t("job.Period6.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company6.1") }}</p>
+          <p>{{ $t("job.Company6.2") }}</p>
+        </td>
         <td>{{ $t("job.Location6") }}</td>
-        <td>{{ $t("job.JobScope6") }}</td>
-        <td>{{ $t("job.Outcome6") }}</td>
-        <td>{{ $t("job.Tools6") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope6.1") }}</p>
+          <p>{{ $t("job.JobScope6.2") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome6.1") }}</p>
+          <p>{{ $t("job.Outcome6.2") }}</p>
+          <p>{{ $t("job.Outcome6.3") }}</p>
+          <p>{{ $t("job.Outcome6.4") }}</p>
+          <p>{{ $t("job.Outcome6.5") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools6.1") }}</p>
+          <p>{{ $t("job.Tools6.2") }}</p>
+          <p>{{ $t("job.Tools6.3") }}</p>
+          <p>{{ $t("job.Tools6.4") }}</p>
+          <p>{{ $t("job.Tools6.5") }}</p>
+          <p>{{ $t("job.Tools6.6") }}</p>
+          <p>{{ $t("job.Tools6.7") }}</p>
+          <p>{{ $t("job.Tools6.8") }}</p>
+        </td>
+      </tr>
+
+      <!--Japan（Tokyo）-->
+      <tr>
+        <td>
+          <p>{{ $t("job.Period7.1") }}</p>
+          <p>{{ $t("job.Period7.2") }}</p>
+          <p>{{ $t("job.Period7.3") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Company7.1") }}</p>
+          <p>{{ $t("job.Company7.2") }}</p>
+        </td>
+        <td>{{ $t("job.Location7") }}</td>
+        <td>
+          <p>{{ $t("job.JobScope7.1") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Outcome7.1") }}</p>
+        </td>
+        <td>
+          <p>{{ $t("job.Tools7.1") }}</p>
+          <p>{{ $t("job.Tools7.2") }}</p>
+          <p>{{ $t("job.Tools7.3") }}</p>
+          <p>{{ $t("job.Tools7.4") }}</p>
+          <p>{{ $t("job.Tools7.5") }}</p>
+          <p>{{ $t("job.Tools7.6") }}</p>
+          <p>{{ $t("job.Tools7.7") }}</p>
+          <p>{{ $t("job.Tools7.8") }}</p>
+          <p>{{ $t("job.Tools7.9") }}</p>
+          <p>{{ $t("job.Tools7.10") }}</p>
+          <p>{{ $t("job.Tools7.11") }}</p>
+        </td>
       </tr>
     </tbody>
   </table>
